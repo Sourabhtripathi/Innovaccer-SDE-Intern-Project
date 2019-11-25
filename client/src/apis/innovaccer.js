24166925689e8811051aaafea435b01da1306036
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2019-11-25 23:59:59 +0530

    client api local to mongodb

--- client/src/apis/innovaccer.js
+++ client/src/apis/innovaccer.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-	// baseURL: 'https://innovaccer-sde-intern-server.herokuapp.com/',
-	baseURL: 'http://localhost:3001',
+	baseURL: 'https://innovaccer-sde-intern-server.herokuapp.com/',
+	// baseURL: 'http://localhost:3001',
 	headers: {
 		'Content-Type': 'application/x-www-form-urlencoded'
 	}
